a76a329de4ed9f27e25987a77527d2d41e2c9ebe
kanuarj <jimmy.lpf@gmail.com>
2023-08-26 14:02:04 +0530

    Front End for NLP Application

---
+++ fullApplication/frontEnd/script.js
@@ -0,0 +1,26 @@
+document.getElementById('formData').addEventListener('submit', predictSpam)
+
+function predictSpam (e) {
+    e.preventDefault()
+
+    let text = document.getElementById('textarea_1').value
+
+    fetch('http://localhost:5000', {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+            'text' : text
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.response == 'spam') {
+            document.getElementById('output').innerHTML = `<div>The message is Spam</div>`
+        }
+        else {
+            document.getElementById('output').innerHTML = `<div>The message is not a Spam</div>`
+        }
+    })
+}
